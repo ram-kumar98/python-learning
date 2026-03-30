4b6e0d1f85513ed0e2961acbe06d0a77dfdda485
ram-kumar98 <rockyrams1998@gmail.com>
2026-03-30 21:18:27 +0530

    Create functions_.py

---
+++ functions_.py
@@ -0,0 +1,23 @@
+# functions
+
+# Advantage of functions is to make code reusable.
+
+# In built functions , User defined functions
+# Defining a function is not alone sufficient to execute it. We have to call the function to execute it.
+
+# syntax 
+
+def wishing():
+    print("Hello, Good Morning")
+    print("Welcome to Python Programming")
+
+wishing() 
+wishing()
+wishing()
+
+
+
+def add_num(a,b): #parameters
+    print(a+b)
+
+add_num(15, -3) #arguments
