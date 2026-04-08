ff02001870d6c50b6749cae0029d3a6dee669f70
ram-kumar98 <rockyrams1998@gmail.com>
2026-04-08 21:33:03 +0530

    Update 13_OOPS.py

--- 13_OOPS.py
+++ 13_OOPS.py
@@ -1,10 +1,8 @@
 # A class variable is a variable that is shared by all objects of the class.
-
 # shared properties = Class Variables
 
 
 # A class method is used to access or modify class variables.
-
 # @classmethod
 # cls 
 
@@ -41,3 +39,55 @@ class OurBank:
 
 
 obj = OurBank('A','TPT',1000)
+
+
+# UNDERSTANDING STATIC METHOD
+
+
+from datetime import datetime
+
+class OurBank:
+
+    def __init__(self,name,place,balance):
+        self.name = name
+        self.place = place 
+        self.total_amount = 0
+        self.balance = balance
+        OurBank.increment_cust_count()
+
+    # CLASS VARIABLES 
+    BankName = 'OurBank Private Limited' 
+    FixedDeposit_InterestRate = 5
+    Total_no_of_customers = 0
+
+    # CLASS METHOD
+    @classmethod
+    def class_method_(cls):
+
+        # Will play with Class variables (We will generally access the instance variables)
+        print("Hey you are enjoying the services of ",cls.BankName)
+        print("We are providing a fixed deposit service with interest rate of ",cls.FixedDeposit_InterestRate)
+
+    @classmethod 
+    def increment_cust_count(cls):
+        cls.Total_no_of_customers = cls.Total_no_of_customers + 1
+        print("Time is -> ",OurBank.get_ts())
+        print("Total no of customers registered in the bank is -> ", cls.Total_no_of_customers)
+
+    # Method inside a class which does not depend on either instance variables or Class variables
+    # It can work with either instance or class level variables to validate or conditional checking
+    # Utility function
+    @staticmethod
+    def get_ts():
+        return datetime.now()
+        
+
+# obj = OurBank('a','TPT',1000)
+
+
+print(OurBank.get_ts())
+
+
+
+
+
